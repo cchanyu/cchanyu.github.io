f5e0d76f2242d2ce34692fd05daf576900c1a645
Cchanyu <Chanyu96@gmail.com>
2021-11-23 09:27:36 -0500

    Fresh Start, boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-import Tile from './components/Tile'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <Tile />
-      </div>
+        hi
     </div>
   );
 }
