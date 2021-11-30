e7d368a6da80958a75d66761f55626ad5183ee4e
Cchanyu <Chanyu96@gmail.com>
2021-11-29 20:54:35 -0500

    Local Timer Set up and Navbar organization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../css/Navbar.css'
+import Wifi from '../icon/wifi-solid.svg';
+import User from '../icon/user-solid.svg';
+import Battery from '../icon/battery-full-solid.svg';
 
 class Navbar extends React.Component {
     constructor(){
@@ -11,11 +14,16 @@ class Navbar extends React.Component {
         return(
             <div className="navbar">
                 <div className="navbar--inside">
-                    <div className="user"></div>
-                    <div className="clock"></div>
-                    <div className="wifi"></div>
-                    <div className="battery"></div>
-                    Navbar
+                    <div className="navbar--inside--left">
+                        <img className="user" src={User} alt="user" />
+                    </div>
+                    <div className="navbar--inside--right">
+                        <div className="clock">
+                            {new Date().toLocaleString("en-US", {hour: '2-digit', hour12: true, minute: '2-digit', timeZone: "America/New_York"})}
+                        </div>
+                        <img className="wifi" src={Wifi} alt="wifi" />
+                        <img className="battery" src={Battery} alt="battery" />
+                    </div>
                 </div>
             </div>
         )
