da8779e2a418c45a5e97cf2c1bd747343582caae
cchanyu <chanyu96@gmail.com>
2023-03-09 17:46:46 -0500

    CC - adjustments on buttons and little details

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { auth } from "../firebase/firebase.config";
 import { getAuth, signOut, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import GoogleIcon from '../icon/google.svg';
 
 function handleSignin(props) {
     const provider = new GoogleAuthProvider();
@@ -30,8 +31,14 @@ export default function Login(props) {
     return (
         <div className='project'>
             {props.isLogged ? 
-            <button type="submit" className="contact--button" onClick={() => handleSignout(props)}>Logout</button> : 
-            <button type="submit" className="contact--button" onClick={() => handleSignin(props)}>Login</button>}
+            <button type="submit" className="contact--button" onClick={() => handleSignout(props)}>Sign out</button> : 
+            <div className='contact--wrap'>
+                <div className='contact--info'>If you want to contact me, please select a sign-in method:</div>
+                <button type="submit" className="contact--button" onClick={() => handleSignin(props)}>
+                    <img className="contact--icon" src={GoogleIcon} alt="." />
+                    <div className="contact--text">Sign in with Google</div>
+                </button>
+            </div>}
         </div>
     )
 }
